a49de724d631b411aea758f6e1038c5ef650650a
kimnayeon <nayeun9198@gmail.com>
2026-05-06 11:22:59 +0900

    feat : Service 클래스, vo 클래스 작성

--- src/main/java/vo/EmployeeVO.java
+++ src/main/java/vo/EmployeeVO.java
@@ -1,4 +1,85 @@
 package vo;
 
+import java.util.Objects;
+
 public class EmployeeVO {
+
+     private String id;
+     private String name;
+     private String postion;
+     private int salary;
+     private String hireDate;
+
+    public EmployeeVO(String id, String name, String postion, int salary, String hireDate) {
+        this.id = id;
+        this.name = name;
+        this.postion = postion;
+        this.salary = salary;
+        this.hireDate = hireDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostion() {
+        return postion;
+    }
+
+    public void setPostion(String postion) {
+        this.postion = postion;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "EmployeeVO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", postion='" + postion + '\'' +
+                ", salary=" + salary +
+                ", hireDate='" + hireDate + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeVO that = (EmployeeVO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
